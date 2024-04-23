f5ddc5537a98f5f57bd51c8e80c30c7bb3ce38dc
luis Dania <css.luism@gmail.com>
2024-04-23 10:16:45 +0000

    Inicial Commit

---
+++ kccapp1/webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("kccapp1.controller.App", {
+        onInit: function() {
+        }
+      });
+    }
+  );
+  
